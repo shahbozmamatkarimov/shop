1e1b7ac3c2a2a46dc953dec467dc1162c0fda3d0
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-06-27 09:20:55 +0300

    title

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,5 @@
 <template>
   <!-- component -->
-  <!-- follow me on twitter @asad_codes -->
-
   <div class="flex flex-wrap fixed z-50">
     <section class="relative mx-auto">
       <!-- navbar -->
@@ -229,22 +227,6 @@
         </a>
       </nav>
     </section>
-  </div>
-  <!-- Does this resource worth a follow? -->
-  <div class="absolute bottom-0 right-0 mb-4 mr-4 z-10">
-    <div>
-      <a
-        title="Follow me on twitter"
-        href="https://www.twitter.com/asad_codes"
-        target="_blank"
-        class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
-      >
-        <img
-          class="object-cover object-center w-full h-full rounded-full"
-          src="https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2019/12/twitter-logo.jpg"
-        />
-      </a>
-    </div>
   </div>
 </template>
 
